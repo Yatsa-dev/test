301e6dee86ef869dae48f46b5260b7d07d71d112
Ihor Yatsyshyn <74863323+Yatsa-dev@users.noreply.github.com>
2022-06-15 22:03:04 +0300

    update users by id

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -2,12 +2,14 @@ import {
   Body,
   Controller,
   Get,
+  Param,
+  Patch,
   Post,
   UseGuards,
   UsePipes,
   ValidationPipe,
 } from '@nestjs/common';
-import { JwtAuthGuard } from 'src/guards/jwt-auth.guard';
+// import { JwtAuthGuard } from 'src/guards/jwt-auth.guard';
 import { UserDTO } from 'src/types/dto/users.dto';
 import { UserService } from './users.service';
 
@@ -21,7 +23,19 @@ export class UsersController {
     const user = await this.userService.create(body);
     return user;
   }
-  @UseGuards(JwtAuthGuard)
+
+  @Patch(':id')
+  @UsePipes(new ValidationPipe())
+  async updateUser(@Param('id') currentUserId: string, @Body() body: UserDTO) {
+    return await this.userService.update(currentUserId, body);
+  }
+
+  @Get(':id')
+  async getUserById(@Param('id') currentUserId: string) {
+    return await this.userService.getUserById(currentUserId);
+  }
+
+  // @UseGuards(JwtAuthGuard)
   @Get()
   async getAllUsers(): Promise<object> {
     return await this.userService.getUsers();
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { UserDTO } from 'src/types/dto/users.dto';
@@ -10,12 +10,28 @@ export class UserService {
 
   async create(user: UserDTO) {
     const newUser = new this.userModel({ ...user });
-    const result = await newUser.save();
-    return result;
+    return await newUser.save();
+  }
+
+  async getUserById(currentUserId: string) {
+    let user: UserDTO;
+    try {
+      user = await this.userModel.findById(currentUserId);
+    } catch (error) {
+      throw new NotFoundException('Could not find user.');
+    }
+    if (!user) {
+      throw new NotFoundException('Could not find user.');
+    }
+    return user;
+  }
+
+  async update(currentUserId: string, user: UserDTO) {
+    await this.userModel.findByIdAndUpdate(currentUserId, user);
+    return user;
   }
 
   async getUsers() {
-    const result = await this.userModel.find();
-    return result;
+    return await this.userModel.find();
   }
 }
